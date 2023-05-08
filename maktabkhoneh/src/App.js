a658bf81d370d748f4601d5bd781bca6f3c6e3df
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-05-08 11:39:55 +0430

    01

---
+++ maktabkhoneh/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
